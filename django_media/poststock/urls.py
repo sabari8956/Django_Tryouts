2dcc431dd82609ece63258a3edab321dc7d6ba1f
Sabari K <sabari8956@gmail.com>
2023-12-26 17:46:46 +0530

    Login, Logout, register Done With Profile Feature

--- django_media/poststock/urls.py
+++ django_media/poststock/urls.py
@@ -4,7 +4,8 @@ from django.conf.urls.static import static
 from .views import *
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("logout", logout_view, name="logout"),
+    path("register", register_view, name="register"),
+    path("login", login_view, name="login"),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- django_media/poststock/views.py
+++ django_media/poststock/views.py
@@ -1,6 +1,56 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.contrib.auth import logout, login,authenticate
+from django.urls import reverse
+from django.db import IntegrityError
+from .models import *
 # Create your views here.
 
 def index(request):
     return render(request, "index.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("/")
+
+def register_view(request):
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST["username"]
+        email = request.POST["email"]
+        # Ensure password matches confirmation
+        password = request.POST["password"]
+        # confirmation = request.POST["confirmation"]
+        file = request.FILES.get('profile_img')
+        # Attempt to create new user
+        try:
+            user = User.objects.create_user(username=username , email=email, password=password, profile_img=file)
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "register.html", {
+                "message": "username already taken."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "register.html")
+    
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+
+        else:
+            return render(request, "login.html", {
+                "message": "Invalid username and/or password."
+            })
+    return render(request, "login.html")
